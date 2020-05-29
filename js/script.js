8182db801b1c8086d4c1357677aff85b5c5fe096
Eli Halpern <groovemachine@processor8.home>
2020-05-28 22:40:51 -0700

    temp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello, front end');
+/* var F = (num - 32) * .556;
+var C = (num * 1.8) + 32;
+*/
+
+document.getElementById('myForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log("I am clcking a button")
+    var temperature = document.getElementById('temperature').value
+    highTemp = parseInt((temperature) - "32") * ".556";
+    document.getElementById("result").innerText=highTemp
+}
+)
+
+// copied this bit from caitlin
+
+function convertTemperature() {
+let farenheitTemp = document.getElementById("F").checked
+let celsiusTemp = document.getElementById("C").checked
+let typedTemperature = document.getElementById("temperature").checked
+if (farenheitTemp == true) {
+    result = (typedTemperature - "32") * ".556";
+} else if (celsiusTemp == true) {
+    result = (typedTemperature * "1.8") + "32";
+} 
+}
